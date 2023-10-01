7b8ba6f92f9583c30d89c06b8f5a5dc43864f1d5
fukuda <fukuda.m0113@icloud.com>
2023-10-01 16:25:10 +0900

    add sagemath implementation

---
+++ python/sagemath/bls12.py
@@ -0,0 +1,134 @@
+from sage.all_cmdline import *  # import sage library
+
+from sage.rings.integer_ring import ZZ
+from sage.rings.rational_field import QQ
+from sage.misc.functional import cyclotomic_polynomial
+from sage.rings.finite_rings.finite_field_constructor import FiniteField, GF
+from sage.schemes.elliptic_curves.constructor import EllipticCurve
+
+# this is much much faster with this statement:
+# proof.arithmetic(False)
+from sage.structure.proof.all import arithmetic
+
+from parameter import find_curve_parameter_b, find_twist_curve_parameter_xi_ab
+from test_pairing import test_order, test_ate_pairing_bls12_aklgl
+
+
+def test_curve(u0):
+    # preparse("QQx.<x> = QQ[]")
+    QQx = QQ["x"]
+    (x,) = QQx._first_ngens(1)
+    # BLS12 polynomials
+    px = (x - 1) ** 2 * (x**4 - x**2 + 1) / 3 + x
+    rx = x**4 - x**2 + 1
+    tx = x + 1
+    cx = (x - 1) ** 2 / 3
+    yx = (x - 1) * (2 * x**2 - 1) / 3
+    c2x = (
+        x**8
+        - 4 * x**7
+        + 5 * x**6
+        - 4 * x**4
+        + 6 * x**3
+        - 4 * x**2
+        - 4 * x
+        + 13
+    ) / 9  # cofactor for G2
+    p = ZZ(px(u0))
+    r = ZZ(rx(u0))
+    c = ZZ(cx(u0))
+    c2 = ZZ(c2x(u0))
+    t = ZZ(tx(u0))
+    y = ZZ(yx(u0))
+    Fp = GF(p, proof=False)
+    k = 12
+    b, E = find_curve_parameter_b(Fp, r, c)
+
+    print("u = {:#x}".format(u0))
+    print("p = {:#x}, {} mod 4 # {} bits".format(p, p % 4, p.nbits()))
+    print("r = {:#x} # {} bits".format(r, r.nbits()))
+    print("c = {:#x} # {} bits".format(c, c.nbits()))
+    print("y = {:#x}".format(y))
+    print("t = {:#x}".format(t))
+    print("BLS{}-{} E: y^2 = x^3 {:+d}".format(k, p.nbits(), b))
+    print("test E (G1): ", end="")
+    test_order(E, r * c)
+
+    # define Fp2
+    Fpz = Fp["z"]
+    (z,) = Fpz._first_ngens(1)
+    if (p % 4) == 3:
+        Fp2 = Fp.extension(z**2 + 1, names=("i",))
+        (i,) = Fp2._first_ngens(1)
+        a = -1
+        print("Fp2 = Fp[x]/(x^2 + 1)")
+    else:
+        a = 2
+        while not (z**2 - a).is_irreducible():
+            a = a + 1
+        print("Fp2 = Fp[x]/(x^2 - {})".format(a))
+        Fp2 = Fp.extension(z**2 - a, names=("i",))
+        (i,) = Fp2._first_ngens(1)
+
+    # Fq = Fp2
+    Fp2s = Fp2["s"]
+    (s,) = Fp2s._first_ngens(1)
+    E2, xi, btw, D_twist = find_twist_curve_parameter_xi_ab(b, Fp2, r, d=6)
+    print("test E' (G2): ", end="")
+    test_order(E2, r * c2)
+    if D_twist:
+        print(
+            "BLS{}-{} E'(Fp4): y^2 = x^3 {:+d}/({}): D-twist".format(
+                k, p.nbits(), b, xi
+            )
+        )
+    else:
+        print(
+            "BLS{}-{} E'(Fp4): y^2 = x^3 {:+d}*({}): M-twist".format(
+                k, p.nbits(), b, xi
+            )
+        )
+
+    Fq6 = Fp2.extension(s**6 - xi, names=("w",))
+    (w,) = Fq6._first_ngens(1)
+    try:
+        coeffs_xi = xi.polynomial().list()
+    except AttributeError as err:
+        coeffs_xi = xi.list()
+    i0 = ZZ(coeffs_xi[0])
+    i1 = ZZ(coeffs_xi[1])
+    if abs(i0 - p) < abs(i0):
+        i0 = i0 - p
+    if abs(i1 - p) < abs(i1):
+        i1 = i1 - p
+
+    # xi = i0 + i*i1
+    # s^6 - xi = 0 <=> s^6 - i0 = i1*i <=> (s^6 - i0)^2 = i1^2*a
+    poly = (z**6 - i0) ** 2 - i1**2 * a
+    Fp12 = Fp.extension(poly, names=("S",))
+    (S,) = Fp12._first_ngens(1)
+    # E_Fp12 = EllipticCurve([Fp12(0), Fp12(b)])
+    print("Fq6 = Fq[s]/(s^6{:+d}{:+d}*i): Fq = Fp2".format(int(-i0), int(-i1)))
+    print("Fp12 = Fp[z]/({})".format(poly))
+
+    def map_Fq6_Fp12(x):
+        # evaluate elements of Fp12 = Fp[i]/(i^2+1)[s]/(s^6-(i+1)) at i=S^6-1 and s=S
+        # i <-> w^6-1 = S^6-1 and w <-> S
+        # return sum([sum([yj*(S**6-1)**j for j,yj in enumerate(xi.polynomial())]) * S**i for i,xi in enumerate(x.list())])
+        return sum(
+            [
+                xi.polynomial()((S**6 - i0) / i1) * S**e
+                for e, xi in enumerate(x.list())
+            ]
+        )
+
+    test_ate_pairing_bls12_aklgl(E, E2, r, c, c2, u0, Fq6, map_Fq6_Fp12, D_twist)
+
+
+# bls12-381
+u0 = ZZ(-(2**63 + 2**62 + 2**60 + 2**57 + 2**48 + 2**16))
+test_curve(u0)
+
+# # bls12-446
+# u0 = ZZ(-(2**75 - 2**73 + 2**63 + 2**57 + 2**50 + 2**17 + 1))
+# test_curve(u0)
---
+++ python/sagemath/bls24.py
@@ -0,0 +1,197 @@
+from sage.all_cmdline import *  # import sage library
+
+from sage.rings.integer_ring import ZZ  # 整数
+from sage.rings.rational_field import QQ  # 有理数
+from sage.misc.functional import cyclotomic_polynomial
+from sage.rings.finite_rings.finite_field_constructor import FiniteField, GF
+from sage.schemes.elliptic_curves.constructor import EllipticCurve
+
+from parameter import (
+    find_curve_parameter_b,
+    find_twist_curve_parameter_xi_ab,
+)
+from test_pairing import test_order, test_ate_pairing_bls24_aklgl
+
+
+def test_curve(u0):
+    print("u0 = {:#x}".format(u0))
+    # preparse("QQx.<x> = QQ[]")
+    QQx = QQ["x"]
+    (x,) = QQx._first_ngens(1)
+    # the problem with BLS24 is the cost of the final exponentiation, and the size of G2.
+    # BLS24 polynomials
+    px = (x - 1) ** 2 * (x**8 - x**4 + 1) / 3 + x
+    rx = x**8 - x**4 + 1
+    tx = x + 1
+    cx = (x - 1) ** 2 / 3
+    yx = (x - 1) * (2 * x**4 - 1) / 3
+    c2x = (
+        x**32
+        - 8 * x**31
+        + 28 * x**30
+        - 56 * x**29
+        + 67 * x**28
+        - 32 * x**27
+        - 56 * x**26
+        + 160 * x**25
+        - 203 * x**24
+        + 132 * x**23
+        + 12 * x**22
+        - 132 * x**21
+        + 170 * x**20
+        - 124 * x**19
+        + 44 * x**18
+        - 4 * x**17
+        + 2 * x**16
+        + 20 * x**15
+        - 46 * x**14
+        + 20 * x**13
+        + 5 * x**12
+        + 24 * x**11
+        - 42 * x**10
+        + 48 * x**9
+        - 101 * x**8
+        + 100 * x**7
+        + 70 * x**6
+        - 128 * x**5
+        + 70 * x**4
+        - 56 * x**3
+        - 44 * x**2
+        + 40 * x
+        + 100
+    ) / 81  # cofactor for G2
+    D = 3  # discriminant (-D = -3)
+    k = 24
+
+    p = ZZ(px(u0))
+    r = ZZ(rx(u0))
+    c = ZZ(cx(u0))
+    c2 = ZZ(c2x(u0))
+    t = ZZ(tx(u0))
+    y = ZZ(yx(u0))
+    Fp = GF(p, proof=False)
+    b, E = find_curve_parameter_b(Fp, r, c)
+
+    print("u = {:#x}".format(u0))
+    print("p = {:#x}, {} mod 4 # {} bits".format(p, p % 4, p.nbits()))
+    print("r = {:#x} # {} bits".format(r, r.nbits()))
+    print("c = {:#x} # {} bits".format(c, c.nbits()))
+    print("y = {:#x}".format(y))
+    print("t = {:#x}".format(t))
+    print("BLS{}-{} E(Fp): y^2 = x^3 {:+d}".format(k, p.nbits(), b))
+    print("test E (G1): ", end="")
+    test_order(E, r * c)
+
+    # define Fp2
+    Fpz = Fp["z"]
+    (z,) = Fpz._first_ngens(1)
+    if (p % 4) == 3:
+        Fp2 = Fp.extension(z**2 + 1, names=("i",))
+        (i,) = Fp2._first_ngens(1)
+        a = -1
+        print("Fp2 = Fp[x]/(x^2 + 1)")
+    else:
+        a = 2
+        while not (z**2 - a).is_irreducible():
+            a = a + 1
+        print("Fp2 = Fp[x]/(x^2 - {})".format(a))
+        Fp2 = Fp.extension(z**2 - a, names=("i",))
+        (i,) = Fp2._first_ngens(1)
+
+    # define Fp4
+    Fp2w = Fp2["w"]
+    (w,) = Fp2w._first_ngens(1)
+    a2 = i
+    while not (w**2 - a2).is_irreducible():
+        a2 = a2 + 1
+    print("Fp4 = Fp2[w]/(w^2 - ({}))".format(a2))
+    # Fp22 = Fp2.extension(w**2 - a2, names=("j",))
+    try:
+        coeffs_a2 = a2.polynomial().list()
+    except AttributeError as err:
+        coeffs_a2 = a2.list()
+    a20 = coeffs_a2[0]
+    a21 = coeffs_a2[1]
+    a20m = ZZ(a20)
+    if abs(a20m - p) < abs(a20m):
+        a20m = a20m - p
+    a21m = ZZ(a21)
+    if abs(a21m - p) < abs(a21m):
+        a21m = a21m - p
+    # w^2 = a21*i+a20 <=> w^2-a20 = a21*i <=> (w^2-a20)^2 = a21*a <=> w^4 -2*a20*w^2 + a20^2 - a21*a = 0
+    Fp4 = Fp.extension(z**4 - 2 * a20 * z**2 + a20**2 - a21 * a, names=("v",))
+    (ii,) = Fp4._first_ngens(1)
+    print(
+        "Fp4 = Fp[z]/(z^4{:+d}*z^2{:+d})".format(
+            int(-2 * a20m), int(a20**2 - a21 * a)
+        )
+    )
+
+    # Fq = Fp4
+    Fp4s = Fp4["s"]
+    (s,) = Fp4s._first_ngens(1)
+    E2, xi, btw, D_twist = find_twist_curve_parameter_xi_ab(b, Fp4, r, d=6)
+    print("test E' (G2): ", end="")
+    test_order(E2, r * c2)
+    if D_twist:
+        print(
+            "BLS{}-{} E'(Fp4): y^2 = x^3 {:+d}/({}): D-twist".format(
+                k, p.nbits(), b, xi
+            )
+        )
+    else:
+        print(
+            "BLS{}-{} E'(Fp4): y^2 = x^3 {:+d}*({}): M-twist".format(
+                k, p.nbits(), b, xi
+            )
+        )
+
+    Fq6 = Fp4.extension(s**6 - xi, names=("w",))
+    (w,) = Fq6._first_ngens(1)
+    try:
+        coeffs_xi = xi.polynomial().list()
+    except AttributeError as err:
+        coeffs_xi = xi.list()
+    i0 = ZZ(coeffs_xi[0])
+    i1 = ZZ(coeffs_xi[1])
+    if abs(i0 - p) < abs(i0):
+        i0 = i0 - p
+    if abs(i1 - p) < abs(i1):
+        i1 = i1 - p
+
+    # w^6 = ii+4 <=> w^6-4 = ii <=> (w^6-4)^4 - 2*(w^6-4)^2 + 2 = 0
+    # w^6 = i1*ii + i0 <=> w^6-i0 = i1*ii => (w^6-i0)^2 = i1^2*(a21*i+a20)
+    #                                         => ((w^6-i0)^2 - i1^2*a20)^2 = i1^4*a21^2 * a
+    poly = ((z**6 - i0) ** 2 - a20 * i1**2) ** 2 - a * a21**2 * i1**4
+    Fp24 = Fp.extension(poly, names=("S",))
+    (S,) = Fp24._first_ngens(1)
+    # E_Fp24 = EllipticCurve([Fp24(0), Fp24(b)])
+    print("Fq6 = Fq[s]/(s^6{:+d}{:+d}*v): Fq = Fp4".format(int(-i0), int(-i1)))
+    print("Fp24 = Fp[z]/({})".format(poly))
+
+    def map_Fq6_Fp24(x, a=S):
+        return sum(
+            [
+                xi.polynomial()((a**6 - i0) / i1) * a**e
+                for e, xi in enumerate(x.list())
+            ]
+        )
+
+    test_ate_pairing_bls24_aklgl(E, E2, r, c, c2, u0, Fq6, map_Fq6_Fp24, D_twist)
+
+
+# # BLS24-315
+# u0 = ZZ(-(2**32) + 2**30 + 2**21 + 2**20 + 1)
+# test_curve(u0)
+
+# BLS24-317
+u0 = ZZ(2**31 + 2**30 + 2**28 + 2**27 + 2**24 + 2**16 + 2**15)
+test_curve(u0)
+
+# # BLS24-318
+# u0 = ZZ(-(2**32) + 2**28 + 2**12)
+# test_curve(u0)
+
+# # BLS24-509
+# u0 = ZZ(-(2**51) - 2**28 + 2**11 - 1)
+# test_curve(u0)
